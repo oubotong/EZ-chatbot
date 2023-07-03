d95785d64e6f28ee48079768da51f52a69a1d5e0
x0oo0x <richard97@g.ucla.edu>
2023-07-03 07:04:04 -0400

    easy gpt chatbot tutorial

---
+++ app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import openai
+
+
+def handle_userinput(user_input):
+
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    with st.chat_message("user", avatar="🦜"):
+        st.markdown(user_input)
+
+    with st.chat_message("assistant", avatar="🧑‍🔧"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+                model=st.session_state["openai_model"],
+                messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+                stream=True,
+            ):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+        
+
+def displayHistoryMsgs():
+    for message in st.session_state.messages:
+        if message["role"] == "user":
+            with st.chat_message("user", avatar="🦜"):
+                st.markdown(message["content"])
+        elif message["role"] == "assistant":
+            with st.chat_message("assistant", avatar="🧑‍🔧"):
+                st.markdown(message["content"])
+
+            
+def init():
+
+
+    # Set OpenAI API key from Streamlit secrets
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+    st.set_page_config(
+        page_title="Your own ChatGPT",
+        page_icon="🤖"
+    )
+    st.header("Your own ChatGPT 🤖")
+
+def main():
+
+    init()
+
+    # Set a default model
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo-0613"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "system", "content": """You are a professional software developer and you will be answering technical 
+                        questions in a professional manner. Still, you need to answer as brief as enough to cover the
+                        important knowledge that the interviewers want to know."""}
+        ]
+    
+    displayHistoryMsgs()
+
+    user_question = st.chat_input("Type your question")
+    if user_question:
+        handle_userinput(user_question)
+
+
+
+if __name__ == '__main__':
+    main()
